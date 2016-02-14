2cc728032719a373d6d112d85cdd30197b4b0a3d
Kirill Kutepov <k.kutepov@ya.ru>
2016-02-15 01:01:31 +0300

    Added README, package.json and scripts MySQL

---
+++ db/inserts_1.sql
@@ -0,0 +1,2 @@
+INSERT INTO `restaurantdb`.`jobcycle` (`workdays`, `daysoff`, `cyclename`) VALUES ('5', '2', 'full-time');
+INSERT INTO `restaurantdb`.`jobcycle` (`workdays`, `daysoff`, `cyclename`) VALUES ('2', '2', 'part-time');
