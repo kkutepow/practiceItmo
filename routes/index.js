f298b89f98584330f998e7a276d80018f6b49b04
Kirill Kutepov <k.kutepov@ya.ru>
2016-02-15 14:31:11 +0300

    Fixed rendering from path '/' to '/adminView/index.ejs'

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,27 @@ var router = express.Router();
 
 /* ======= index.ejs (Главная) ======= */
 router.get('/', function(req, res, next) {
-    res.render('adminView/index.ejs', {
-        title: "Добро пожаловать"
+    var chefCount, restCount;
+    knexSQL('chefs')
+        .select()
+        .count()
+        .then(function(chefs){
+
+        for(var count in chefs[0]) { chefCount = chefs[0][count]; break; }    
+
+        knexSQL('restaurants')
+            .select()
+            .count()
+            .then(function(restaurants){
+
+            for(var count in restaurants[0]) { restCount = restaurants[0][count]; break; }   
+
+            res.render('adminView/index.ejs', {
+                title: "Добро пожаловать",
+                chefCount: chefCount,
+                restCount: restCount
+            });
+        });
     });
 });
 
